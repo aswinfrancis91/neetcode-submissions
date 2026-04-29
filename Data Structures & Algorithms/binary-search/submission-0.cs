c546faed32f0084302f9da97e155bd903e5159b7
Aswin Francis <aswinfrancis91@users.noreply.github.com>
2026-04-29 20:05:33 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.Length - 1;
+        int mid = -1;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            if (target < nums[mid]) {
+                right = mid - 1;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
