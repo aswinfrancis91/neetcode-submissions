5e2673669e1700ddb98a58b855f5ecf5390623ba
Aswin Francis <aswinfrancis91@users.noreply.github.com>
2026-05-11 14:24:08 +0530

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        int left = 1;
+        int right = 1;
+        while (right < nums.Length) {
+            if (nums[right - 1] == nums[right]) {
+                right++;
+            } else {
+                nums[left] = nums[right];
+                left++;
+                right++;
+            }
+        }
+        return left;
+    }
+}
